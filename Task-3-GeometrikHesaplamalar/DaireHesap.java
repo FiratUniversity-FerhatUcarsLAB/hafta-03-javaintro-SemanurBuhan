d52810bb5b3539a1ba0af823a0f1ae4631205322
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 16:14:40 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,20 @@
+package gorev3alanvecevrehesaplamaları;
+
+public class DaireAlanVeCevre {
+    public static void main(String[] args) {
+         
+           double yarıçap = 5.5;
+
+           double pi = 3.14159;
+
+           double alan = pi * yarıçap * yarıçap;
+           
+           double çevre = 2 * pi * yarıçap;
+
+           System.out.println("Yaricap: " + yarıçap);
+           System.out.println("Alan: " + alan);
+           System.out.println("Cevre: " + çevre);
+        
+    }
+    
+}

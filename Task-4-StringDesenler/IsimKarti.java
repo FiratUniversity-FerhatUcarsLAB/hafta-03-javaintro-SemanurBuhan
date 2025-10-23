8a05eff704f96bc2602e9454bd0953fb9e5263e2
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 16:20:27 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+package görev4metinvestring;
+
+public class IsimKartı {
+    
+    public static void main(String[] args) {
+    	        String ad =      "Semanur";
+    	        String soyad="Buhan";
+    	        String ogrNo= "250541007";
+    	        String bolum= "Yazilim Muh.";
+
+    	         System.out.println("===================");
+    	         System.out.println(" OGRENCI BILGILERI ");
+    	         System.out.println("===================");
+    	         System.out.println("Ad:    " +    ad);
+    	         System.out.println("Soyad: " + soyad);
+    	         System.out.println("No:    " +   ogrNo);
+    	         System.out.println("Bolum: "+ bolum);
+    	         System.out.println("===================");
+    	 }
+    	}

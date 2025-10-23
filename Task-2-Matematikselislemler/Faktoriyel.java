6bfb9e60ebe5773b6aed1af601c8787b17916ce2
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 16:07:34 +0300

    Add Faktoriyel class to calculate factorial of a number

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,12 @@
+package gorev2hesaplamalar;
+
+public class Faktoriyel {
+    public static void main(String[] args) {
+    int i,fact=1;
+  int number=5;// our number to do the necessary calculations in class Factorial
+  for(i=1;i<=number;i++){
+      fact=fact*i;
+  }
+  System.out.println("Factorial of "+number+" is: "+fact);
+ }
+}

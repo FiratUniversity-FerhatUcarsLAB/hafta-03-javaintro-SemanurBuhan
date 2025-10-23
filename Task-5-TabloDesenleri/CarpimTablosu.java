5f3a0cee403fe663dab0854267abc6866a71dbfe
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 16:24:01 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,14 @@
+package görev5karmaşıkçıktıformatları;
+
+public class CarpımTablosu {
+    public static void main(String[] args) {
+        System.out.println("Çarpım Tablosu");
+        System.out.println("---------------");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}

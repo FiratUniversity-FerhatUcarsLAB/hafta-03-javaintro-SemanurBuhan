b974365574b09e10c3c442f4b66d47c1ad1b2257
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 15:43:49 +0300

    Create BesKereTekrar.java to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,13 @@
+package gorev1hello;
+
+public class BesKereTekrar {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoşgeldiniz!");
+        System.out.println("Java'ya Hoşgeldiniz!");
+        System.out.println("Java'ya Hoşgeldiniz!");
+        System.out.println("Java'ya Hoşgeldiniz!");
+        System.out.println("Java'ya Hoşgeldiniz!");
+
+    }
+    
+}

60fbef2c08a78e454a899613f203401128e92def
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 16:15:37 +0300

    Add Dikdortgen class for area and perimeter calculation

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+package gorev3alanvecevrehesaplamaları;
+
+public class Dikdortgen {
+    
+    public static void main(String[] args) {
+         double boy = 4.5;
+         double en = 7.9;
+		 double alan = (boy * en);
+         double çevre = 2 * (boy + en);
+         System.out.println("Boy: " + boy);
+         System.out.println("En: " + en);
+         System.out.println("Alan: " + alan);
+         System.out.println("Çevre: " + çevre);
+     }
+}    

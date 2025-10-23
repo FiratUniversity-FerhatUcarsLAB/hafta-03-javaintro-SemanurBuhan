8cfff8ba89cf65de7fcb86c9d2c6271095ab04c6
Semanur Buhan <250541007@firat.edu.tr>
2025-10-23 16:18:09 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,27 @@
+package görev4metinvestring;
+
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("          _____");  
+	        System.out.println("         ¥¥    ¥");
+	        System.out.println("        ¥::¥    ¥");
+	        System.out.println("       ¥::::¥    ¥");
+	        System.out.println("      ¥::::::¥    ¥");
+	        System.out.println("     ¥:::¥¥:::¥    ¥");
+	        System.out.println("    ¥:::¥__¥:::¥    ¥");
+	        System.out.println("    ¥:::¥   ¥:::¥    ¥");
+	        System.out.println("  ___¥:::¥   ¥:::¥    ¥");
+	        System.out.println(" ¥¥   ¥:::¥   ¥:::¥    ¥");
+	        System.out.println("¥::¥   ¥:::¥   ¥:::¥____¥");
+	        System.out.println("¥:::¥   ¥:::¥   ¥::¥    ¥");
+	        System.out.println(" ¥:::¥   ¥:::¥   ¥¥____¥");
+	        System.out.println("  ¥:::¥   ¥:::¥    ¥");
+	        System.out.println("   ¥:::¥   ¥:::¥____¥");
+	        System.out.println("    ¥:::¥  ¥:::¥    ¥");
+	        System.out.println("     ¥:::¥¥:::¥    ¥");
+	        System.out.println("      ¥::::::¥    ¥");
+	        System.out.println("       ¥::::¥    ¥");
+	        System.out.println("        ¥::¥____¥");
+	
+	    }
+	}
